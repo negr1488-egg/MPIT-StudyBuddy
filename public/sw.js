a4e2da8c94a1f132e5e0bbfba4ca5fa832aeae60
MDS <bs4919382@gmail.com>
2026-04-24 22:40:02 +0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', () => {
+  self.clients.claim()
+})
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+
+  event.waitUntil(
+    clients.openWindow('/')
+  )
+})
